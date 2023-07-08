d958fe81a5aba3fa9f509b278b4db4622f22eb1a
Ryan Novak <rnovakdevacc@gmail.com>
2023-07-07 23:36:18 -0400

    Debug user input
    Created a loop to guarantee that user input is restricted to
    integers that correspond to operator options.
    
    Added general comments for readability.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -18,13 +18,30 @@ double Divide(double x, double y)
     return x / y;
 }
 
-
+// print operator choices to user
 Console.WriteLine("Select operator: \n 1. Addition \n 2. Subtraction \n 3. " +
     "Multiplication \n 4. Division");
-// TODO: make this reject non integers (tryparse, returns T/F)
-// TODO: make this reject numbers greater or lesser than possible options
-int opNum = Convert.ToInt16(Console.ReadLine());
 
+// ensure input only accepts integer values, from 1-4
+bool isValid = false;
+int selection = 0;
+while (isValid == false) {
+    // take user selection for operator
+    string? opNum = Console.ReadLine();
+
+    // check if the user input is valid
+    if (int.TryParse(opNum, out selection) && int.Parse(opNum) > 0 && int.Parse(opNum) < 4)
+    {
+        // set condition to break the loop
+        isValid = true;
+    }
+    else
+    {
+        Console.WriteLine("Please input an integer between 1 and 4");
+    }    
+}
+
+// take user input for operands
 Console.WriteLine("x: ");
 string? xStr = Console.ReadLine();
 double x = Convert.ToDouble(xStr);
@@ -33,8 +50,9 @@ Console.WriteLine("y: ");
 string? yStr = Console.ReadLine();
 double y = Convert.ToDouble(yStr);
 
+// TODO: Make this its own "calculate" function for readability
 double answer = 0.0;
-switch (opNum)
+switch (selection)
 {
     case 1:
         answer = Add(x, y);
@@ -52,5 +70,7 @@ switch (opNum)
         Console.WriteLine("Please select a valid operation.");
         break;
 }
+
+// print output to user and wait to close prompt
 Console.WriteLine($"{Convert.ToString(answer)}");
 Thread.Sleep(3000);
